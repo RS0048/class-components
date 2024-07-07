50a9e6d4d1c172b784d564524747eee38dfe51c0
RS0048 <sokolovrmn@gmail.com>
2024-07-07 23:07:07 +0300

    feat: added components of section

--- src/components/topSection/topSection.tsx
+++ src/components/topSection/topSection.tsx
@@ -2,10 +2,12 @@ import { Component } from 'react';
 import './topSection.css';
 import SearchComponent from '../searchComponent/searchComponent';
 
+console.log('test');
 class TopSection extends Component {
   handleSearch = (query: string): void => {
     console.log('Обработка поискового запроса в TopSection:', query);
   };
+
   render(): JSX.Element {
     return (
       <div className="top-section">

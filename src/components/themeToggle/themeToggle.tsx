310e19dffa0af49a78db26256d0e48c5dff0dfee
RS0048 <sokolovrmn@gmail.com>
2024-07-28 23:06:14 +0300

    fix: saved theme to LS

--- src/components/themeToggle/themeToggle.tsx
+++ src/components/themeToggle/themeToggle.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useTheme } from '../../themeContext';
 
 const ThemeToggleButton: React.FC = () => {
   const { isDarkTheme, toggleTheme } = useTheme();
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem('isDarkTheme');
+    if (savedTheme !== null) {
+      if (savedTheme !== String(isDarkTheme)) {
+        toggleTheme();
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('isDarkTheme', String(isDarkTheme));
+  }, [isDarkTheme]);
 
   return (
     <button onClick={toggleTheme}>
